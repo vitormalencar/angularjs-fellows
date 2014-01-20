41b50e652e9b59f88b69a9e6dc2b2995459aa249
tachun lin <creed88@gmail.com>
2014-01-20 09:57:51 +0100

    Fix some css, remove vm

---
+++ javascripts/main.js
@@ -0,0 +1,9 @@
+var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    width = w.innerWidth || e.clientWidth || g.clientWidth,
+    height = w.innerHeight|| e.clientHeight|| g.clientHeight;
+    height = height - 200;
+
+document.getElementById("header").style.height = height+ "px";
